cd670765bb3776fe4ceea7d023642df03379de20
ZeQ61 <zeki14@icloud.com>
2024-11-11 16:04:38 +0300

    admin

---
+++ OrderPayment/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderPayment.Controllers
+{
+    public class AdminController : Controller
+    {
+        // Admin Paneli Sayfası
+        public IActionResult AdminPanel()
+        {
+            return View();  // Admin Paneli görünümüne yönlendirir
+        }
+
+        // Ürün Ekle Sayfası
+        public IActionResult AddProduct()
+        {
+            return View();  // Ürün Ekle görünümüne yönlendirir
+        }
+
+        // Ürün Düzenle Sayfası
+        public IActionResult EditProduct()
+        {
+            return View();  // Ürün Düzenle görünümüne yönlendirir
+        }
+    }
+}
